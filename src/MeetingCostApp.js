8e31139b233bbde20f2c9992d313497c91af9377
Ian Kent <email@iankent.co.uk>
2017-09-14 21:57:23 +0100

    styling

--- src/MeetingCostApp.js
+++ src/MeetingCostApp.js
@@ -17,6 +17,13 @@ import CreateMeeting from './components/CreateMeeting';
 import ViewMeeting from './components/ViewMeeting';
 import JoinMeeting from './components/JoinMeeting';
 
+import IconButton from 'material-ui/IconButton';
+import ActionToday from 'material-ui/svg-icons/action/today';
+import ActionSupervisorAccount from 'material-ui/svg-icons/action/supervisor-account';
+import ActionAccountBox from 'material-ui/svg-icons/action/account-box';
+
+import ReactGridLayout from 'react-grid-layout';
+
 import uuidv4 from 'uuid/v4';
 
 class App extends Component {
@@ -43,10 +50,7 @@ class App extends Component {
     };
 
     Home() {
-        return <div>
-            <RaisedButton containerElement={<Link to="/create" />} type="submit" label="Create meeting" primary={true} />
-            <RaisedButton containerElement={<Link to="/join" />} type="submit" label="Join meeting" primary={false} />
-        </div>;
+        return <div></div>;
     };
 
     User() {
@@ -94,34 +98,52 @@ class App extends Component {
                     <AppBar
                         title={<span>Meeting Cost</span>}
                         onLeftIconButtonTouchTap={this.toggleMenu}
-                        showMenuIconButton={this.state.userExists}
+                        showMenuIconButton={false}
+                        iconElementRight={ 
+                            !this.state.userExists ? 
+                            "" :
+                            <div>
+                                <IconButton 
+                                    containerElement={<Link to="/create" />} 
+                                    type="submit" 
+                                    label="Create" 
+                                    primary={true} 
+                                ><ActionToday /></IconButton>
+                                <IconButton 
+                                    containerElement={<Link to="/join" />} 
+                                    type="submit" 
+                                    label="Join" 
+                                    secondary={true} 
+                                ><ActionSupervisorAccount /></IconButton>
+                                <IconButton 
+                                    containerElement={<Link to="/user" />} 
+                                    type="submit" 
+                                    label="User" 
+                                ><ActionAccountBox /></IconButton>
+                            </div>
+                         }
                     />
-                    <Drawer 
-                            open={this.state.menuOpen} 
-                            docked={false}
-                            onRequestChange={(open) => this.setState({menuOpen: open})}
-                        >
-                        <MenuItem containerElement={<Link to="/create" />} onClick={this.handleClose}>Create meeting</MenuItem>
-                        <MenuItem containerElement={<Link to="/join" />} onClick={this.handleClose}>Join meeting</MenuItem>
-                        <MenuItem containerElement={<Link to="/user" />} onClick={this.handleClose}>Edit user</MenuItem>
-                    </Drawer>
 
                     { !this.state.userExists ? <Redirect to="/user"/> : "" }
                 
-                    <Switch>
-                        <Route exact path="/" component={this.Home} />
-                        <Route exact path="/user" component={this.User} />
-                        <Route exact path="/join" component={JoinMeeting} />
-                        <Route exact path="/create" render={ props => (
-                            <CreateMeeting user={this.state.user} />
-                        )} />
-                        <Route exact path="/meeting/:id" render={ props => (
-                            <ViewMeeting user={this.state.user} />
-                        )} />
-                        <Route>
-                            <div>Page not found</div>
-                        </Route>
-                    </Switch>
+                    <div style={{marginTop: 48, marginBottom: 48, marginLeft: 72, marginRight: 72}}>
+                        <Switch>
+                            <Route exact path="/" render={ props => (
+                                <Redirect to="/create"/>
+                            )} />
+                            <Route exact path="/user" component={this.User} />
+                            <Route exact path="/join" component={JoinMeeting} />
+                            <Route exact path="/create" render={ props => (
+                                <CreateMeeting user={this.state.user} />
+                            )} />
+                            <Route exact path="/meeting/:id" render={ props => (
+                                <ViewMeeting user={this.state.user} />
+                            )} />
+                            <Route>
+                                <div>Page not found</div>
+                            </Route>
+                        </Switch>
+                    </div>
                 </div>
             </Router>
         );
